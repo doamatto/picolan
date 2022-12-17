1d91b837973475585a5a805b5bfce041a45b7426
Matthew Ronchetto <hello@doamatto.xyz>
2022-12-17 07:34:18 -0800

    chore: comment 'broken' things and blink LED when using

--- sensor.py
+++ sensor.py
@@ -1,29 +1,31 @@
 import network
 import time
-from machine import Pin
+from machine import Pin, Timer
 
 # enable station interface
 nic = network.WLAN(network.AP_IF)
 nic.config(
-	ssid= 'PicoLAN',
-	channel= 11,
-	hidden= false,
-	security= 4,
-	key= "tarmac nappy manual reoccupy"
-)
+	ssid='PicoLAN',
+	#channel=11,
+	#hidden=False,
+	#security=CYW43_AUTH_WPA2_AES_PSK,
+	key='ronchetto')
 nic.ifconfig(('10.0.10.1','255.255.0.0', '10.0.10.1', '1.1.1.1'))
 nic.active(True)
-led = machine.Pin("LED", machine.Pin.OUT)
-led.toggle()
+
+# blink LED
+tim = Timer()
+led = machine.Pin('LED', machine.Pin.OUT)
+def tick(timer):
+	global led
+	led.toggle()
+tim.init(freq=2.5, mode=Timer.PERIODIC, callback=tick)
 
 # nic.config(pm) # Debugging
 
-while True:
-	if nic.isconnected():
+#while True:
+	#if nic.isconnected():
 		# if it's being used, we don't need to be so "eco-friendly"
-		nic.config(pm= 0xa11140)
-	else:
+		#nic.config(pm= 0xa11140)
+	#else:
 		#nic.config(pm= ???) # need to use Pico to get initial pm value
-
-
-
